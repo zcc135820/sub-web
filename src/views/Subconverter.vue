6e90ac9f5bc1ba6001e7f39fe04204cc6744b855
zcc135820 <2438676624@qq.com>
2021-12-30 20:09:31 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -255,7 +255,7 @@ export default {
           ClashR: "clashr",
           Surge2: "surge&ver=2",
         },
-        backendOptions: [{ value: "https://subweb-heroku.yunbai.workers.dev/sub?" }],
+        backendOptions: [{ value: "https://old-dust-c727.zhang890515888.workers.dev/sub?" }],
         remoteConfig: [
         {
             label: "ACL4SSR",
